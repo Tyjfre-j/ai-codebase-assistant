d796827aa35bcdb5e2c650b1ec2e53a07402b9a3
Tyjfre-j <bouhamzaabouda@gmail.com>
2026-07-02 23:27:01 +0100

    feat: added go queries

---
+++ app/ingestion/languages/go_queries.py
@@ -0,0 +1,35 @@
+GO_QUERY = """
+(function_declaration
+  name: (identifier) @func.name
+  parameters: (parameter_list) @func.params
+) @func.def
+
+(method_declaration
+  receiver: (parameter_list) @func.receiver
+  name: (field_identifier) @func.name
+  parameters: (parameter_list) @func.params
+) @func.def
+
+(type_declaration
+  (type_spec
+    name: (type_identifier) @class.name
+    type: (struct_type) @class.body
+  )
+) @class.def
+
+(import_declaration
+  (import_spec
+    name: (package_identifier)? @import.alias
+    path: (interpreted_string_literal) @import.path
+  )
+) @import.stmt
+
+(import_declaration
+  (import_spec_list
+    (import_spec
+      name: (package_identifier)? @import.alias
+      path: (interpreted_string_literal) @import.path
+    )
+  )
+) @import.stmt
+"""
